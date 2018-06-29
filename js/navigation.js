6ed6d6da4e5df7cd986f374182a6786f29611c15
Julian Bolliger <julian.bolliger@ticketino.com>
2018-06-29 17:12:05 +0200

    Arrows were added, because the key-control doesnt work on every computer.

--- js/navigation.js
+++ js/navigation.js
@@ -101,7 +101,15 @@ function isElementInViewport (el) {
       rect.top == 0 &&
       rect.left == 0 &&
       rect.bottom == (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right == (window.innerWidth || document.documentElement.clientWidth
-      )
+      rect.right == (window.innerWidth || document.documentElement.clientWidth)
     );
 }
+
+/*
+$(document).ready(function(){
+  $(".imgArrowRight").on("click", function(){
+    console.log("click");
+    swipe("center", "left");
+  });
+});
+*/
